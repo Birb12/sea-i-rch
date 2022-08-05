ba73122ba9e940a308d22084581b67375a4d03f9
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-05 14:20:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,65 +4,50 @@ from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.datasets
+
+def savemodel():
+    path = "./savedmodel.pth"
+    torch.save(model.state_dict(), path) # state dict is current weights and biases
+
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(NN, self).__init__()
+        self.layer1 = nn.Linear(input_size, 50) 
+        self.layer2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = self.layer2(x)
+        return x
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_size = 784
+num_grids = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 1
+
+dataset = torchvision.datasets.MNIST(root='dataset/', train=True, transform=torchvision.transforms.ToTensor()) # placeholder
+dataload = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+model = NN(input_size, num_classes=num_grids).to(device) 
+criterion = nn.CrossEntropyLoss() 
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for data, targets in dataload:
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+        data = data.reshape(data.shape[0], -1) 
+        scores = model(data)
+        loss = criterion(scores, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(loss)
 
 
-# Input (temp, rainfall, humidity)
-inputs = np.array([[73, 67, 43], 
-                   [91, 88, 64], 
-                   [87, 134, 58], 
-                   [102, 43, 37], 
-                   [69, 96, 70], 
-                   [74, 66, 43], 
-                   [91, 87, 65], 
-                   [88, 134, 59], 
-                   [101, 44, 37], 
-                   [68, 96, 71], 
-                   [73, 66, 44], 
-                   [92, 87, 64], 
-                   [87, 135, 57], 
-                   [103, 43, 36], 
-                   [68, 97, 70]], 
-                  dtype='float32')
-
-# Targets (apples, oranges)
-targets = np.array([[56, 70], 
-                    [81, 101], 
-                    [119, 133], 
-                    [22, 37], 
-                    [103, 119],
-                    [57, 69], 
-                    [80, 102], 
-                    [118, 132], 
-                    [21, 38], 
-                    [104, 118], 
-                    [57, 69], 
-                    [82, 100], 
-                    [118, 134], 
-                    [20, 38], 
-                    [102, 120]], 
-                   dtype='float32')
-
-inputs = torch.from_numpy(inputs)
-targets = torch.from_numpy(targets)
-
-def fit(mse, num_epochs, optimizer):
-    for i in range (num_epochs):
-        for xb, yb in DataLoadTrainer:
-            preds = model(xb)
-            loss = mse(preds, yb)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            print(loss)
-
-
-
-model = nn.Linear(3, 2)
-
-mse = F.mse_loss
-optimizer = torch.optim.SGD(model.parameters(), lr = 1e-5)
-
-dataset = TensorDataset(inputs, targets)
-DataLoadTrainer = DataLoader(dataset, batch_size = 5, shuffle = True)
-
-fit(mse, 500, optimizer)
