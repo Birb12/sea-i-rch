95941582bbc342da9ef3b68e87f08c643c34163f
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-08 16:15:04 -0700

    Create normalization.py

---
+++ normalization.py
@@ -0,0 +1,31 @@
+import os
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+path = os.path.join(os.getcwd(), "training")
+transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+dataset = torchvision.datasets.ImageFolder(path, transform)
+loader = DataLoader(dataset, batch_size = 32, shuffle = True)
+
+def get_mean_and_std(loader):
+    mean = 0
+    std = 0
+    total_images = 0
+
+    for images, _ in loader:
+        image_count_in_batch = images.size(0)
+        images = images.view(image_count_in_batch, images.size(1), -1)
+        mean += images.mean(2).sum(0)
+        std += images.std(2).sum(0)
+        total_images += image_count_in_batch
+
+    mean /= total_images 
+    std /= total_images
+    print(mean)
+    print(std)
+
+    return mean, std
+
+get_mean_and_std(loader)
