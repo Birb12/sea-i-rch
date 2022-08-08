674d62f2b05d4f6c82ac91a7d0b94c3c89802403
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-07 18:18:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,29 @@
-import numpy as np
-import torch
-from torch.utils.data import TensorDataset
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
-import torchvision.datasets
+import torch
+import torchvision.transforms as transforms
+import os
+import torch.utils.data
+import matplotlib.pyplot as plt
+import numpy as np
 
-def savemodel():
-    path = "./savedmodel.pth"
-    torch.save(model.state_dict(), path) # state dict is current weights and biases
+transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
 
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(NN, self).__init__()
-        self.layer1 = nn.Linear(input_size, 50) 
-        self.layer2 = nn.Linear(50, num_classes)
+data_direction = os.getcwd()
+trainingdata = os.path.join(data_direction, "training")
 
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = self.layer2(x)
-        return x
+train_dataset = torchvision.datasets.ImageFolder(trainingdata, transform)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-input_size = 784
-num_output = 2
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 1
+def show_images(train_dataset):
+    loader = torch.utils.data.DataLoader(train_dataset, batch_size = 50, shuffle=True)
+    batch = next(iter(loader))
+    images, labels = batch
 
-dataset = torchvision.datasets.MNIST(root='dataset/', train=True, transform=torchvision.transforms.ToTensor()) # placeholder
-dataload = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+    grid = torchvision.utils.make_grid(images, nrow=3)
+    plt.figure(figsize=(11,11))
+    plt.imshow(np.transpose(grid, (1, 2, 0)))
+    plt.show()
+    print('labels: ', labels)
 
-model = NN(input_size, num_classes=num_output).to(device) 
-criterion = nn.CrossEntropyLoss() 
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-for epoch in range(num_epochs):
-    for data, targets in dataload:
-        data = data.to(device=device)
-        targets = targets.to(device=device)
-        data = data.reshape(data.shape[0], -1) 
-        scores = model(data)
-        loss = criterion(scores, targets)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        print(loss)
-
+show_images(train_dataset)
 
