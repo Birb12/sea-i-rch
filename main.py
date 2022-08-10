d1b218e90bee6e04e0865ca4827c268fe9169366
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-09 21:03:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class ConvNetwork(nn.Module):
     def __init__(self):
 
@@ -16,15 +17,14 @@ class ConvNetwork(nn.Module):
         self.layer1 = nn.Conv2d(3, 6, 5)
         self.poollayer = nn.MaxPool2d(2, 2)
         self.layer2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*61*61, 40000)
-        self.fc2 = nn.Linear(40000, 14884)
-        self.fc3 = nn.Linear(14884, 2)
+        self.fc1 = nn.Linear(16*61*61, 15000)
+        self.fc2 = nn.Linear(15000, 10000)
+        self.fc3 = nn.Linear(10000, 2)
 
     def forward(self, x):
 
         x = self.poollayer(F.relu(self.layer1(x)))
         x = self.poollayer(F.relu(self.layer2(x)))
-        print(x.shape)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -38,7 +38,7 @@ custommean = 0.8132, 0.6343, 0.7334
 customstd = 0.0807, 0.1310, 0.0968
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 1e-5)
+optimizer = torch.optim.SGD(model.parameters(), lr = 1e-5)
 
 
 transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(), transforms.Normalize(custommean, customstd)])
@@ -47,7 +47,7 @@ outputs = ("1", "0")
 
 numepoch = 2
 data_direction = os.getcwd()
-trainingdata = os.path.join(data_direction, "training")
+trainingdata = os.path.join(data_direction, "test-train")
 
 train_dataset = torchvision.datasets.ImageFolder(trainingdata, transform)
 loader = torch.utils.data.DataLoader(train_dataset, batch_size = 100, shuffle=True)
@@ -65,6 +65,7 @@ def train():
             optimizer.step()
             optimizer.zero_grad()
             print(loss)
+    
 
 
 def show_images(train_dataset):
@@ -76,6 +77,14 @@ def show_images(train_dataset):
     plt.imshow(np.transpose(grid, (1, 2, 0)))
     plt.show()
     print('labels: ', labels)
+
+def save():
+
+    torch.save({
+        'modelstates' : model.state_dict(),
+        "optimizerstates": optimizer.state_dict()
+    }, '/checkpoint.pth')
+
 
 
 train()
