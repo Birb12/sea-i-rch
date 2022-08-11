6fe09d7610f14b9976cde53d83a391806a0fd9c1
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-10 19:27:17 -0700

    Update and rename main.py to train.py

--- train.py
+++ train.py
@@ -17,9 +17,9 @@ class ConvNetwork(nn.Module):
         self.layer1 = nn.Conv2d(3, 6, 5)
         self.poollayer = nn.MaxPool2d(2, 2)
         self.layer2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*61*61, 15000)
-        self.fc2 = nn.Linear(15000, 10000)
-        self.fc3 = nn.Linear(10000, 2)
+        self.fc1 = nn.Linear(1296, 100)
+        self.fc2 = nn.Linear(100, 20)
+        self.fc3 = nn.Linear(20, 2)
 
     def forward(self, x):
 
@@ -34,20 +34,22 @@ class ConvNetwork(nn.Module):
         
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = ConvNetwork().to(device)
+
 custommean = 0.8132, 0.6343, 0.7334
 customstd = 0.0807, 0.1310, 0.0968
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
 
-transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(), transforms.Normalize(custommean, customstd)])
+transform = transforms.Compose([transforms.Resize((50, 50)), transforms.ToTensor(), transforms.RandomRotation(degrees=(-20, +20)), transforms.Normalize(custommean, customstd)])
 outputs = ("1", "0")
 
 
-numepoch = 2
+numepoch = 7
 data_direction = os.getcwd()
 trainingdata = os.path.join(data_direction, "test-train")
+least_loss = 1
 
 train_dataset = torchvision.datasets.ImageFolder(trainingdata, transform)
 loader = torch.utils.data.DataLoader(train_dataset, batch_size = 100, shuffle=True)
@@ -57,14 +59,19 @@ def train():
         for images, labels in loader:
             images = images.to(device)
             labels = labels.to(device)
+            global least_loss
 
             outputs = model(images)
             loss = criterion(outputs, labels)
 
+            if (loss < least_loss):
+                least_loss = loss
+                save()
+
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            print(loss)
+            print(loss.item())
     
 
 
@@ -80,11 +87,8 @@ def show_images(train_dataset):
 
 def save():
 
-    torch.save({
-        'modelstates' : model.state_dict(),
-        "optimizerstates": optimizer.state_dict()
-    }, '/checkpoint.pth')
-
+    torch.save(model.state_dict(), 'current-best.pth')
+    print("model saved")
 
 
 train()
