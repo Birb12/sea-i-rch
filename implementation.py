c88d49e2bbbaa86f507ba8e374d331df5f3287d3
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-21 10:55:21 -0700

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -11,180 +11,83 @@ from PIL import Image
 from math import ceil
 
 
-base_model = [
-    [1, 16, 1, 1, 3],
-    [6, 24, 2, 2, 3],
-    [6, 40, 2, 2, 5],
-    [6, 80, 3, 2, 3],
-    [6, 112, 3, 1, 5],
-    [6, 192, 4, 2, 5],
-    [6, 320, 1, 1, 3],
-]
-
-phi_values = {
-    "b0": (0, 224, 0.2),  
-    "b1": (0.5, 240, 0.2),
-    "b2": (1, 260, 0.3),
-    "b3": (2, 300, 0.3),
-    "b4": (3, 380, 0.4),
-    "b5": (4, 456, 0.4),
-    "b6": (5, 528, 0.5),
-    "b7": (6, 600, 0.5),
+change = {
+    'curr': [32, 'M', 64, 'M', 128, 128, 'M', 256, 'M', 512, 'M'],
 }
 
-class CNNBlock(nn.Module):
-    def __init__(
-            self, in_channels, out_channels, kernel_size, stride, padding, groups=1
-    ):
-        super(CNNBlock, self).__init__()
-        self.cnn = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            groups=groups,
-            bias=False,
-        )
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.silu = nn.SiLU() 
 
-    def forward(self, x):
-        return self.silu(self.bn(self.cnn(x)))
-
-class SqueezeExcitation(nn.Module):
-    def __init__(self, in_channels, reduced_dim):
-        super(SqueezeExcitation, self).__init__()
-        self.se = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1), 
-            nn.Conv2d(in_channels, reduced_dim, 1),
-            nn.SiLU(),
-            nn.Conv2d(reduced_dim, in_channels, 1),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x):
-        return x * self.se(x)
-
-class InvertedResidualBlock(nn.Module):
-    def __init__(
-            self,
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            expand_ratio,
-            reduction=4, 
-            survival_prob=0.8, 
-    ):
-        super(InvertedResidualBlock, self).__init__()
-        self.survival_prob = 0.8
-        self.use_residual = in_channels == out_channels and stride == 1
-        hidden_dim = in_channels * expand_ratio
-        self.expand = in_channels != hidden_dim
-        reduced_dim = int(in_channels / reduction)
-
-        if self.expand:
-            self.expand_conv = CNNBlock(
-                in_channels, hidden_dim, kernel_size=3, stride=1, padding=1,
+class make_cnn(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000):
+        super(make_cnn, self).__init__()
+        self.in_channels = in_channels
+        self.conv_layers = self.create_conv_layers(change['curr'])
+        
+        self.fcs = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 2048),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, num_classes)
             )
-
-        self.conv = nn.Sequential(
-            CNNBlock(
-                hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim,
-            ),
-            SqueezeExcitation(hidden_dim, reduced_dim),
-            nn.Conv2d(hidden_dim, out_channels, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-        )
-
-    def stochastic_depth(self, x):
-        if not self.training:
-            return x
-
-        binary_tensor = torch.rand(x.shape[0], 1, 1, 1, device=x.device) < self.survival_prob
-        return torch.div(x, self.survival_prob) * binary_tensor
-
-    def forward(self, inputs):
-        x = self.expand_conv(inputs) if self.expand else inputs
-
-        if self.use_residual:
-            return self.stochastic_depth(self.conv(x)) + inputs
-        else:
-            return self.conv(x)
-
-
-class EfficientNet(nn.Module):
-    def __init__(self, version, num_classes):
-        super(EfficientNet, self).__init__()
-        width_factor, depth_factor, dropout_rate = self.calculate_factors(version)
-        last_channels = ceil(1280 * width_factor)
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.features = self.create_features(width_factor, depth_factor, last_channels)
-        self.classifier = nn.Sequential(
-            nn.Dropout(dropout_rate),
-            nn.Linear(last_channels, num_classes),
-        )
-
-    def calculate_factors(self, version, alpha=1.2, beta=1.1):
-        phi, res, drop_rate = phi_values[version]
-        depth_factor = alpha ** phi
-        width_factor = beta ** phi
-        return width_factor, depth_factor, drop_rate
-
-    def create_features(self, width_factor, depth_factor, last_channels):
-        channels = int(32 * width_factor)
-        features = [CNNBlock(3, channels, 3, stride=2, padding=1)]
-        in_channels = channels
-
-        for expand_ratio, channels, repeats, stride, kernel_size in base_model:
-            out_channels = 4*ceil(int(channels*width_factor) / 4)
-            layers_repeats = ceil(repeats * depth_factor)
-
-            for layer in range(layers_repeats):
-                features.append(
-                    InvertedResidualBlock(
-                        in_channels,
-                        out_channels,
-                        expand_ratio=expand_ratio,
-                        stride = stride if layer == 0 else 1,
-                        kernel_size=kernel_size,
-                        padding=kernel_size//2, 
-                    )
-                )
-                in_channels = out_channels
-
-        features.append(
-            CNNBlock(in_channels, last_channels, kernel_size=1, stride=1, padding=0)
-        )
-
-        return nn.Sequential(*features)
-
+        
     def forward(self, x):
-        x = self.pool(self.features(x))
-        return self.classifier(x.view(x.shape[0], -1))
+        x = self.conv_layers(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fcs(x)
+        return x
+
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+        
+        for x in architecture:
+            if type(x) == int:
+                out_channels = x
+                
+                layers += [nn.Conv2d(in_channels=in_channels,out_channels=out_channels,
+                                     kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU()]
+                in_channels = x
+            elif x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
+                
+        return nn.Sequential(*layers)
+
+        
 
 
-custommean = 0.7455, 0.5832, 0.6751
-customstd = 0.2143, 0.2036, 0.2033
+
+class dualchannel(nn.Module):
+    def __init__(self):
+        super(dualchannel, self).__init__()
+        self.ConvNetA = make_cnn()
+        self.ConvNetB = make_cnn()
+    def forward(self, x):
+        x1 = self.ConvNetA.forward(x)
+        x2 = self.ConvNetB.forward(x)
+
+        x = torch.cat((x1, x2), dim=1)
+        return x
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((50, 50)), transforms.Normalize(custommean, customstd), transforms.RandomRotation(degrees=(-20, +20))])
-version = "b0"
-phi, res, drop_rate = phi_values[version]
-num_examples, num_classes = 2, 2
-model = EfficientNet(
-        version=version,
-        num_classes=num_classes,
-    ).to(device)
-
-classes = ("0", "1")
-model.load_state_dict(torch.load('current-best.pth'))
+custommean = 0.8132, 0.6343, 0.7334
+customstd = 0.0807, 0.1310, 0.0968
+numepoch = 8
 data_direction = os.getcwd()
+transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize(custommean, customstd)])
 trainingdata = os.path.join(data_direction, "test-train")
+classes = ("0", "1")
+
 train_dataset = torchvision.datasets.ImageFolder(trainingdata, transform)
-loader = torch.utils.data.DataLoader(train_dataset, batch_size=5, shuffle=True)
+loader = torch.utils.data.DataLoader(train_dataset, batch_size = 100, shuffle=True)
+
+model = make_cnn(in_channels=3, num_classes=2).to(device)
+model.load_state_dict(torch.load("bestdual.pth"))
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+criterion = nn.CrossEntropyLoss()
 
 def classify(model, imagetransforms, imagepath, classes):
     model = model.eval()
@@ -196,4 +99,4 @@ def classify(model, imagetransforms, imagepath, classes):
     _, predicted = torch.max(output.data, 1)
     print(predicted.item())
 
-classify(model, transform, imagepath=r'C:\Users\mined\Desktop\projects\torch\test-train\internet5.jpg', classes=classes)
+classify(model, transform, imagepath=r'C:\Users\mined\Desktop\projects\torch\test-train\1\8863_idx5_x1201_y901_class1.png', classes=classes)
