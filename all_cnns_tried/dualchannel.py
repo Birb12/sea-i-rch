0d2ba76893069043025da85e2eb643b5a5ca447d
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-08-21 10:42:01 -0700

    Create dualchannel.py

---
+++ all_cnns_tried/dualchannel.py
@@ -0,0 +1,110 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import torchvision
+
+
+change = {
+    'curr': [32, 'M', 64, 'M', 128, 128, 'M', 256, 'M', 512, 'M'],
+}
+
+
+class make_cnn(nn.Module):
+    def __init__(self, in_channels=3, num_classes=1000):
+        super(make_cnn, self).__init__()
+        self.in_channels = in_channels
+        self.conv_layers = self.create_conv_layers(change['curr'])
+        
+        self.fcs = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 2048),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, num_classes)
+            )
+        
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fcs(x)
+        return x
+
+    def create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+        
+        for x in architecture:
+            if type(x) == int:
+                out_channels = x
+                
+                layers += [nn.Conv2d(in_channels=in_channels,out_channels=out_channels,
+                                     kernel_size=(3,3), stride=(1,1), padding=(1,1)),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU()]
+                in_channels = x
+            elif x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
+                
+        return nn.Sequential(*layers)
+
+        
+
+
+
+class dualchannel(nn.Module):
+    def __init__(self):
+        super(dualchannel, self).__init__()
+        self.ConvNetA = make_cnn()
+        self.ConvNetB = make_cnn()
+    def forward(self, x):
+        x1 = self.ConvNetA.forward(x)
+        x2 = self.ConvNetB.forward(x)
+
+        x = torch.cat((x1, x2), dim=1)
+        return x
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+custommean = 0.8132, 0.6343, 0.7334
+customstd = 0.0807, 0.1310, 0.0968
+numepoch = 8
+data_direction = os.getcwd()
+transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize(custommean, customstd)])
+trainingdata = os.path.join(data_direction, "test-train")
+
+train_dataset = torchvision.datasets.ImageFolder(trainingdata, transform)
+loader = DataLoader(train_dataset, batch_size = 100, shuffle=True)
+
+model = make_cnn(in_channels=3, num_classes=2).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+criterion = nn.CrossEntropyLoss()
+least = 1
+
+
+def train():
+    for epoch in range(numepoch):
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            preds = model(images)
+            loss = criterion(preds, labels)
+            global least
+
+            if (loss < least):
+                save()
+                least = loss
+            
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            print(loss.item())
+
+def save():
+    print("saved")
+    torch.save(model.state_dict(), "bestdual.pth")
+
+train()
