5744075878c77c1a893083db3eaa95e589a21421
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-10-16 12:02:46 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,1 +1,153 @@
-/
+from distutils.cmd import Command
+import tkinter
+import tkinter.messagebox
+import customtkinter
+import sys
+from tkinter import filedialog
+import implementation
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+import torch.nn as nn
+import os
+from implementation import make_cnn
+
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+prediction = "Waiting..."
+
+
+
+
+class App(customtkinter.CTk):
+
+    WIDTH = 600
+    HEIGHT = 400
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Search")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
+        self.resizable(0, 0)
+        
+
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=0)   # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="Search Beta",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                        values=["Light", "Dark", "System"],
+                                                        command=self.change_appearance_mode)
+        self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+        # ============ frame_right ============
+
+        # configure grid layout (3x7)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_right.rowconfigure(7, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+
+        # ============ frame_info ============
+
+        # configure grid layout (1x1)
+        self.frame_info.rowconfigure(0, weight=1)
+        self.frame_info.columnconfigure(0, weight=1)
+
+        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+                                                   text="Waiting for input..",
+                                                   height=100,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT)
+        self.label_info_1.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
+
+        def browse():
+            print("ran")
+            filename = filedialog.askopenfilename(initialdir="/", title = "Browse Files", filetypes = (("Png Files", "*.png"), ("Jpg files", "*.jpg")))
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            custommean = 0.8132, 0.6343, 0.7334
+            customstd = 0.0807, 0.1310, 0.0968
+            data_direction = os.getcwd()
+            transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize(custommean, customstd)])
+
+            classes = ("0", "1")
+
+
+            model = make_cnn(in_channels=3, num_classes=2).to(device)
+            model.load_state_dict(torch.load("bestdual.pth"))
+
+            prediction = implementation.classify(model, transform, imagepath=filename, classes=classes)
+            prediction = str(prediction)
+
+            if prediction == "0":
+                prediction += ": Cancer Negative"
+            elif prediction == "1":
+                prediction += ": Cancer Positive"
+            self.label_info_1.configure(text=prediction)
+
+
+        # ============ frame_right ============
+
+        self.button_5 = customtkinter.CTkButton(master=self.frame_right,
+                                                width = 10,
+                                                text="Browse Files",
+                                                height = 30,
+                                                corner_radius=3,
+                                                fg_color=("white", "gray38"),
+                                                command = browse)
+                                                
+        self.button_5.grid(row=8, column=2, columnspan=1, pady=20, padx=20, sticky="we")
+
+        # set default values
+        self.optionmenu_1.set("Dark")
+
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
